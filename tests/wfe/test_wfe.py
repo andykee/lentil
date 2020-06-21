78885200b93f7fa047d6689d71f0de46b4080f81
Andy Kee <andykee@gmail.com>
2020-06-20 23:17:29 -0700

    wfe unit test

---
+++ tests/wfe/test_wfe.py
@@ -0,0 +1,13 @@
+import numpy as np
+import lentil
+
+
+def test_translation_defocus():
+    mask = lentil.util.circlemask((256, 256), 128)
+    f_number = np.random.normal(10)
+    translation = np.random.uniform(low=-0.5e-3, high=0.5e-3)
+
+    phase = lentil.wfe.translation_defocus(mask, f_number, translation)
+    pv_defocus = translation/(8*f_number**2)
+
+    assert pv_defocus == np.max(phase) - np.min(phase)
