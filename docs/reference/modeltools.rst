62c0441623b5ab962f8e23216fb33b0b9461b44e
Andy Kee <andykee@gmail.com>
2021-08-12 09:35:14 -0700

    Remove dead modeltools code (resolves #30)

--- docs/reference/modeltools.rst
+++ docs/reference/modeltools.rst
@@ -8,7 +8,4 @@ Model helper functions (``lentil.modeltools``)
     :toctree: ../generated/
 
     lentil.modeltools.normalize_power
-    lentil.modeltools.iterable_amplitude
-    lentil.modeltools.iterable_mask
-    lentil.modeltools.iterable_phase
-    lentil.modeltools.iterable_segmask
+    
--- lentil/modeltools.py
+++ lentil/modeltools.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-__all__ = ['normalize_power', 'iterable_amplitude', 'iterable_mask', 'iterable_phase',
-           'iterable_segmask']
+__all__ = 'normalize_power'
 
 
 def normalize_power(array, power=1):
@@ -38,61 +37,3 @@ def normalize_power(array, power=1):
     """
     array = np.asarray(array)
     return array * np.sqrt(power/np.sum(np.abs(array)**2))
-
-
-def iterable_amplitude(iterable):
-    """Construct a common amplitude array from an iterable of planes
-
-    Parameters
-    ----------
-    iterable : list_like
-        List of planes
-
-    Returns
-    -------
-    amplitude : ndarray
-
-    """
-    amplitude = np.array(1)
-    for plane in iterable:
-        amplitude = amplitude * plane.amplitude
-    return amplitude
-
-
-def iterable_phase(iterable):
-    """Construct a common phase array from an iterable of planes
-
-    Parameters
-    ----------
-    iterable: list_like
-        List of planes
-
-    Returns
-    -------
-    phase: ndarray
-
-    """
-
-    phase = np.array(0)
-    for plane in iterable:
-        phase = phase + plane.phase
-    return phase
-
-
-def iterable_mask(iterable):
-    """Construct a common mask array from an iterable of planes
-
-    Parameters
-    ----------
-    iterable: list_like
-        List of planes
-
-    Returns
-    -------
-    mask: ndarray
-
-    """
-    mask = np.array(1)
-    for plane in iterable:
-        mask = mask * plane.mask
-    return mask
