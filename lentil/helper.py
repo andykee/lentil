Fix off by one error

lentil/helper.py
@@ -129,7 +129,7 @@ def slice_offset(slice, shape):
         else:
             raise ValueError(f"Can't compute offset from slice {slice}")
     else:
-        slice_shape = np.array((slice[0].stop-slice[0].start-1, slice[1].stop-slice[1].start-1))
+        slice_shape = np.array((slice[0].stop-slice[0].start, slice[1].stop-slice[1].start))
         slice_center = slice_shape//2
 
         shape = np.asarray(shape)

tests/test_helper.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 import lentil
 import lentil.util
@@ -24,11 +25,16 @@ def test_boundary_slice():
 
     assert (slice(rmin, rmax), slice(cmin, cmax)) == slc
 
-
-def test_slice_offset():
+@pytest.mark.parametrize('shape, radius', [
+    ((256, 256), 256//4),
+    ((256, 256), 256//5),
+    ((255, 255), 256//4),
+    ((255, 255), 256//5)
+])
+def test_slice_offset(shape, radius):
     shift = np.random.uniform(low=-50, high=50, size=2).astype(int)
 
-    a = lentil.circlemask((256, 256), 256//4, shift=shift)
+    a = lentil.circlemask(shape=shape, radius=radius, shift=shift)
     slc = lentil.helper.boundary_slice(a)
     offset = lentil.helper.slice_offset(slc, shape=a.shape)
 